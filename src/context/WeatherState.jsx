1afe699703d54a9681bfa131e0d63a4d6642968d
AlexWeb07 <kaushikkumardas1@gmail.com>
2023-12-17 15:41:04 +0530

    solving errors

--- src/context/WeatherState.jsx
+++ src/context/WeatherState.jsx
@@ -9,17 +9,17 @@ function WeatherState(props) {
     const getWeather = async(city)=>{
         try {
         // gettin latitude and longitude
-        // const locationData=await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},in&limit=1&appid=${apiKey}`)
-        // const locationRes=await locationData.json();
-        // const lat=locationRes[0].lat;
-        // const lon=locationRes[0].lon;
-        // const location= locationRes[0].name;
+        const locationData=await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city},in&limit=1&appid=${apiKey}`)
+        const locationRes=await locationData.json();
+        const lat=locationRes[0].lat;
+        const lon=locationRes[0].lon;
+        const location= locationRes[0].name;
         
             // getting weather
-        const weatherData=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=26.2967719&lon=73.0351433&appid=c4c22b097afbe2cc62c66c2eb2c7f1ec`)
+        const weatherData=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
         const weatherRes= await weatherData.json();
         setWeather({
-            location:"jodhpur",
+            location,
             title:weatherRes.weather[0].main,
             desc:weatherRes.weather[0].description,
             icon:`https://openweathermap.org/img/wn/${weatherRes.weather[0].icon}@4x.png`,
