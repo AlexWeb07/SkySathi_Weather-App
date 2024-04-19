ef24756f8fb9421cf8e5b9bf47c95a463a0f3d13
AlexWeb07 <kaushikkumardas1@gmail.com>
2024-04-19 12:25:19 +0530

    fixed city error in weather state

--- src/context/WeatherState.jsx
+++ src/context/WeatherState.jsx
@@ -2,37 +2,42 @@ import React, { useState } from 'react'
 import WeatherContext from './WeatherContext'
 
 function WeatherState(props) {
-    const[city,setCity]=useState();
+    const[city,setCity]=useState("");
     const [weather,setWeather]=useState({location:"",title:"",desc:"",icon:"",temp:null,fl:null,humid:null,visibility:null,wind_speed:null,time:"",result:true});
     const apiKey = import.meta.env.VITE_API_KEY;
 
 
     const getWeather = async(city)=>{
-        try {
-        // gettin latitude and longitude
-        const locationData=await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},in&limit=1&appid=${apiKey}`)
-        const locationRes=await locationData.json();
-        const lat=locationRes[0].lat;
-        const lon=locationRes[0].lon;
-        const location= locationRes[0].name;
-    
-        // getting weather
-        const weatherData=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
-        const weatherRes= await weatherData.json();
-        setWeather({
-            location,
-            title:weatherRes.weather[0].main,
-            desc:weatherRes.weather[0].description,
-            icon:`https://openweathermap.org/img/wn/${weatherRes.weather[0].icon}@4x.png`,
-            temp:(weatherRes.main.temp-273.15).toFixed(2),
-            fl:(weatherRes.main.feels_like-273.15).toFixed(2),
-            humid:weatherRes.main.humidity,
-            visibility:weatherRes.visibility/1000,
-            wind_speed:weatherRes.wind.speed,
-        });
-        } catch (error) {
-            alert("Enter valid city name!!");
+        if(city!==""){
+            try {
+                // gettin latitude and longitude
+                const locationData=await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city},in&limit=1&appid=${apiKey}`)
+                const locationRes=await locationData.json();
+                const lat=locationRes[0].lat;
+                const lon=locationRes[0].lon;
+                const location= locationRes[0].name;
+            
+                // getting weather
+                const weatherData=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`)
+                const weatherRes= await weatherData.json();
+                setWeather({
+                    location,
+                    title:weatherRes.weather[0].main,
+                    desc:weatherRes.weather[0].description,
+                    icon:`https://openweathermap.org/img/wn/${weatherRes.weather[0].icon}@4x.png`,
+                    temp:(weatherRes.main.temp-273.15).toFixed(2),
+                    fl:(weatherRes.main.feels_like-273.15).toFixed(2),
+                    humid:weatherRes.main.humidity,
+                    visibility:weatherRes.visibility/1000,
+                    wind_speed:weatherRes.wind.speed,
+                });
+                } catch (error) {
+                    alert("Enter valid city name!!");
+                }
         }
+        // else{
+        //     alert("Enter valid city name!!"); 
+        // }
     }
 
   return (
